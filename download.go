37eefd7eee1778954dddefd2a259910e501a7a9e
shavit <shavit@users.noreply.github.com>
2017-11-14 22:15:20 -0500

    🙈

--- download.go
+++ download.go
@@ -17,7 +17,15 @@ type ticket struct {
 
 type link struct {}
 
-type download struct {}
+type download struct {
+  Name string `json: "name"`
+  Size int `json: "size"`
+  Sha1 string `json: "sha1"`
+  ContentType string `json: "content_type"`
+  UploadAt string `json: "upload_at"`
+  Url string `json: "url"`
+  Token string `json: "token"`
+}
 
 // GetDownloadTicket preparing a download
 func (ol *openload) GetDownloadTicket(fileId string) (tckt *ticket, err error) {
@@ -44,5 +52,23 @@ func (ol *openload) GetDownloadTicket(fileId string) (tckt *ticket, err error) {
 
 // GetDownloadLink get a download link by using a download ticket
 func (ol *openload) GetDownloadLink(fileId string, tckt *ticket, captcha string) (downld *download, err error) {
+  resp, err := ol.get(fmt.Sprintf("/file/dl?file=%v&ticket=%v&captcha_response=%v", fileId, tckt.Ticket, captcha))
+  if err != nil {
+    return downld, err
+  }
+
+  if resp.Status != 200 {
+    return downld, errors.New(resp.Msg)
+  }
+
+  body, err := json.Marshal(resp.Result)
+  if err != nil {
+    return downld, err
+  }
+
+  if err = json.Unmarshal(body, &downld); err != nil {
+    return downld, err
+  }
+
   return downld, err
 }
--- download_test.go
+++ download_test.go
@@ -15,7 +15,7 @@ func TestGetDownloadTicket(t *testing.T){
     t.Error("Error getting a ticket for file Id", fileId, err.Error())
   }
 
-  if tickt == nil {
+  if tickt.Ticket == "" {
     t.Error("Found nil ticket for fileId", fileId)
   }
 
@@ -30,7 +30,11 @@ func TestGetDownloadLink(t *testing.T){
   var captcha string = ""
 
   downld, err = ol.GetDownloadLink(fileId, tickt, captcha)
-  if err != nil || downld != nil {
+  if err != nil {
     t.Error("Error getting a download link:", err.Error())
+  }
+
+  if downld == nil {
+    t.Error("Found nil download for fileid", fileId)
   }
 }
